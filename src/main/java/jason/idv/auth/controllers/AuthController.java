eb53977aa8aa32ceb4a56eecbae9c64f04647fc8
Jason <st890670@gmail.com>
2022-05-15 03:44:02 +0800

    chore: add Swagger UI
    add swagger for interacting with API

--- src/main/java/jason/idv/auth/controllers/AuthController.java
+++ src/main/java/jason/idv/auth/controllers/AuthController.java
@@ -1,5 +1,6 @@
 package jason.idv.auth.controllers;
 
+import io.swagger.annotations.ApiOperation;
 import jason.idv.auth.facades.AuthFacade;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,6 +12,7 @@ public class AuthController {
 
   @Autowired private AuthFacade authFacade;
 
+  @ApiOperation("密碼驗證")
   @PostMapping("/verify")
   public boolean verifyPassword(@RequestParam String password) {
     return authFacade.verifyPassword(password);
